5ca5af92b501eceb700fffb0d837bb2d93b5471b
chostler24 <connor.hostler@gmail.com>
2023-09-05 13:44:49 -0500

    task 2

---
+++ Webpack/task_2/webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+    entry: './js/dashboard_main.js', // Adjust the entry point as needed
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'public'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/, // This rule will match .css files
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|jpg|jpeg|gif|svg)$/, // This rule will match image files
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            outputPath: 'images/', // Output path for images
+                        },
+                    },
+                ],
+            },
+        ],
+    },
+};
