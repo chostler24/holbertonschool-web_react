7b7ac24b2dfb7730a1be4b9f3a45c471c8ea3fdc
chostler24 <connor.hostler@gmail.com>
2023-09-05 12:42:17 -0500

    removed jquery script from index.html

--- Webpack/task_0/src/index.js
+++ Webpack/task_0/src/index.js
@@ -1,17 +1,3 @@
 import $ from "jquery";
 
-// Function to add paragraphs to the page body
-function addParagraphs() {
-    // Create three different paragraphs
-    const paragraph1 = $('<p>').text('Holberton Dashboard');
-    const paragraph2 = $('<p>').text('Dashboard data for the students');
-    const paragraph3 = $('<p>').text('Copyright - Holberton School');
-
-    // Append the paragraphs to the body of the page
-    $('body').append(paragraph1, paragraph2, paragraph3);
-}
-
-// Call the function to add paragraphs when the document is ready
-$(document).ready(function() {
-    addParagraphs();
-});
+["$('body').append('<p>Holberton Dashboard</p>');", "$('body').append('<p>Dashboard data for the students</p>');", "$('body').append('<p>Copyright - Holberton School</p>');"]
