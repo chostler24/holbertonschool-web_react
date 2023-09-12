3f0638151317a6ade655999fcda6f56f8a5f2227
chostler24 <connor.hostler@gmail.com>
2023-09-12 14:18:09 -0500

    Adding mock folder for jest, updated all folders

---
+++ react_props/task_0/dashboard/src/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { checkIsIndex, getFooterCopy, getFullYear } from '../utils/utils';
+import './Footer.css';
+
+
+const Footer = () => (
+    <div className="App-footer">
+      <p>Copyright {getFullYear()} - {getFooterCopy(checkIsIndex())}</p>
+    </div>
+  );
+
+export default Footer();
---
+++ react_props/task_0/dashboard/src/Header/Header.js
@@ -0,0 +1,13 @@
+import logo from '../assets/logo.jpg';
+import React from 'react';
+import './Header.css';
+
+
+const Header = () => (
+    <div className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h1 className="title">School dashboard</h1>
+    </div>
+  );
+
+export default Header();
