8271ce69b7ae9a7a5a1ebb9a35f9561055906091
chostler24 <connor.hostler@gmail.com>
2023-10-07 15:19:19 -0500

    task 0 creation, adding provided files

---
+++ react_redux_connectors_and_providers/task_0/dashboard/src/actions/notificationActionCreators.js
@@ -0,0 +1,26 @@
+import { MARK_AS_READ, SET_TYPE_FILTER, NotificationTypeFilters } from "./notificationActionTypes";
+import { bindActionCreators } from 'redux';
+
+// Action creator function to mark a notification as read
+export function markAsRead(index) {
+  return {
+    type: MARK_AS_READ, // Action type: MARK_AS_READ
+    index, // Index of the notification to mark as read
+  };
+};
+
+// Action creator function to set the notification type filter
+export function setTypeFilter(filter) {
+  return {
+    type: SET_TYPE_FILTER, // Action type: SET_TYPE_FILTER
+    filter: filter, // The notification type filter to set
+  };
+};
+
+// Binding action creators to be used in the application
+export const boundNotificationsActionCreators = bindActionCreators(
+  {
+    markAsRead,
+    setTypeFilter,
+  }
+);
